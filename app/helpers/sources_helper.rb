1901b332089e9ba69e409ce594e32737df7d5834
katycarr <khcarr9@gmail.com>
2018-05-01 14:22:11 -0400

    updated search to remove spaces

--- app/helpers/sources_helper.rb
+++ app/helpers/sources_helper.rb
@@ -1,8 +1,8 @@
 module SourcesHelper
   def search(term)
-    matchable_term = term.downcase
+    matchable_term = term.downcase.gsub(/\s+/, '')
     @source = Source.find do |source|
-      source.name.downcase.gsub(/\s+/, '-').include?(matchable_term) or source.url.downcase.include?(matchable_term)
+      source.name.downcase.gsub(/\s+/, '').include?(matchable_term) or source.url.downcase.include?(matchable_term)
     end
     @source
   end
